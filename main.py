b94e066d5d8b36be875b0cdf4933716b7ea5950e
Rishab Kumar <rishabkumar7@gmail.com>
2023-09-07 10:19:18 -0400

    Added langchain logic

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import streamlit as st
-import langchain as lch
+import langchain_helper as lch
+import textwrap
 
 st.title("YouTube Assistant")
 
 youtube_url = st.sidebar.text_area(
     label="What is the YouTube video URL?",
-    max_chars=30
+    max_chars=50
     )
 
-question = st.sidebar.text_area(
+query = st.sidebar.text_area(
     label="Ask me about the video?",
     max_chars=50
     )
+
+if query:
+    db = lch.create_db_from_youtube_video_url(youtube_url)
+    response, docs = lch.get_response_from_query(db, query)
+    st.subheader("Answer:")
+    st.text(textwrap.fill(response, width=85))
