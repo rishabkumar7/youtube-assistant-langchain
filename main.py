e569518a4012734a1df4002bf539b0706acc31a9
Rishab Kumar <rishabkumar7@gmail.com>
2023-09-18 11:08:39 -0400

    updated the sidebar to include OpenAI key as var

--- main.py
+++ main.py
@@ -4,18 +4,33 @@ import textwrap
 
 st.title("YouTube Assistant")
 
-youtube_url = st.sidebar.text_area(
-    label="What is the YouTube video URL?",
-    max_chars=50
-    )
+with st.sidebar:
+    with st.form(key='my_form'):
+        youtube_url = st.sidebar.text_area(
+            label="What is the YouTube video URL?",
+            max_chars=50
+            )
+        query = st.sidebar.text_area(
+            label="Ask me about the video?",
+            max_chars=50,
+            key="query"
+            )
+        openai_api_key = st.sidebar.text_input(
+            label="OpenAI API Key",
+            key="langchain_search_api_key_openai",
+            max_chars=50,
+            type="password"
+            )
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+        "[View the source code](https://github.com/rishabkumar7/pets-name-langchain/tree/main)"
+        submit_button = st.form_submit_button(label='Submit')
 
-query = st.sidebar.text_area(
-    label="Ask me about the video?",
-    max_chars=50
-    )
-
-if query:
-    db = lch.create_db_from_youtube_video_url(youtube_url)
-    response, docs = lch.get_response_from_query(db, query)
-    st.subheader("Answer:")
-    st.text(textwrap.fill(response, width=85))
+if query and youtube_url:
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+    else:
+        db = lch.create_db_from_youtube_video_url(youtube_url)
+        response, docs = lch.get_response_from_query(db, query)
+        st.subheader("Answer:")
+        st.text(textwrap.fill(response, width=85))
